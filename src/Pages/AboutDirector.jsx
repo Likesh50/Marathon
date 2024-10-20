43957549b12d4f318c20e0da46d945f3700f1308
likesh50 <like22050.it@rmkec.ac.in>
2024-10-20 23:07:15 +0530

    modifications

--- src/Pages/AboutDirector.jsx
+++ src/Pages/AboutDirector.jsx
@@ -7,15 +7,18 @@ const AboutDirector = () => {
     <div className="about-director-container">
       <div className="about-director">
         <div className="about-content">
-          <h1>Our Director</h1>
+          <h1>Beloved Director</h1>
           <h2>Dr. M. Suresh Babu</h2>
+          <h6 style={{marginTop:"-3%"}}>Trustee & Director</h6>
+          <h6 >Sripuram Golden Temple</h6>
+          <h6 style={{marginBottom:"6%"}}> & Sri Narayani Schools</h6>
           <p>
-          Dr. M. Suresh Babu, the Trustee and Director of Sripuram Golden Temple and Sri Narayani Schools, has been rendering dedicated divine service for over 35 years alongside the revered Sri Sakthi Amma. A successful entrepreneur, Dr. Suresh Babu has contributed significantly to various social and spiritual causes, working closely with Sri Sakthi Amma to uplift the community.           </p>
+          Dr. M. Suresh Babu, has been rendering dedicated divine service for over 35 years alongside the revered Sri Sakthi Amma. A successful entrepreneur, Dr. Suresh Babu has contributed significantly to various social and spiritual causes, working closely with Sri Sakthi Amma to uplift the community.           </p>
           <p>
-          He is also a champion of environmental sustainability, having founded the "Green Sakthi" initiative, which focuses on planting trees and donating saplings to the public to promote greener surroundings. In celebration of his birthday this year, a grand marathon is being organized on November 17th, reflecting his commitment to health, well-being, and community spirit. His leadership and contributions have left a lasting impact on both the educational and social sectors.          </p>
+          He is also a champion of environmental sustainability, having founded the "Green Sakthi" initiative, which focuses on planting trees and donating saplings to the public to promote greener surroundings.<br></br> <b>In celebration of his birthday this year (21 November)</b>, a grand marathon is being organized on November 17th, reflecting his commitment to health, well-being, and community spirit. His leadership and contributions have left a lasting impact on both the educational and social sectors.          </p>
         </div>
         <div className="director-image">
-          <img src={directorImage} alt="Director" />
+          <img src={directorImage} style={{borderRadius:"20px 0px 30px 20px"}} alt="Director" />
         </div>
       </div>
     </div>
